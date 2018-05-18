3b0b83e8dbba2f2c9241224dedb2e257c3019015
gprangel <39166045+gprangel@users.noreply.github.com>
2018-05-18 12:41:53 -0300

    Added course validations

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,12 @@
 class Course < ApplicationRecord
+  
+    validates :name, presence: {message: 'this field cannot be left black'}
+    
+    validates :credits, numericality: true, only_integer: true,
+                        presence: {message: 'this field cannot be left black'}
+    
+    validates :faculty, presence: {message: 'this field cannot be left black'}
+    
+    validates :expected_time, numericality: true, only_integer: true
+  
 end
