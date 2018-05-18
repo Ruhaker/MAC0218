c2b5fb6b866138d34d105c0df559bc41c59f0ee5
gprangel <39166045+gprangel@users.noreply.github.com>
2018-05-18 15:21:23 -0300

    Added student validations

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,6 +4,13 @@ class Student < ApplicationRecord
     has_many :course, :through => :plans
 =======
 class Student < User
+    
+    validates :nusp, numericality: true, only_integer: true,
+                        presence: {message: 'this field cannot be left black'},
+                        format: {with: /[0-9]{7,8}/,
+                                 message: 'is not a valid No. USP'},
+                        uniqueness: {message: 'this No. USP already belongs to an user'}
+    
 >>>>>>> c71314fb0a529138718b22799cfce377bf1cbdb9
 end
 
