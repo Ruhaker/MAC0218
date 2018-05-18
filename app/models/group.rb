c71314fb0a529138718b22799cfce377bf1cbdb9
gprangel <39166045+gprangel@users.noreply.github.com>
2018-05-18 14:23:38 -0300

    Added group validations

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,9 @@
 class Group < ApplicationRecord
+    
+    validates :name, presence: {message: 'this field cannot be left black'}
+    
+    validates :min_credits, numericality: true, only_integer: true
+  
+    validates :min_subjects, numericality: true, only_integer: true
+  
 end
