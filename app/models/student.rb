a21b9faa8307f77f7a1676a0bebe21c2076e4db1
alas <akasolak@gmail.com>
2018-06-07 19:03:49 -0300

    Student tests
    Signed-off-by: alas <akasolak@gmail.com>
    Signed-off-by: gprangel <gabriely.pereira@usp.br>

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,11 +1,15 @@
 class Student < User
-    has_and_belongs_to_many :subjects, :through => :subject_students
-    has_and_belongs_to_many :courses, :through => :plans
+    has_and_belongs_to_many :subjects, :through => :subject_students, :join_table => :subject_students
+    has_and_belongs_to_many :courses, :through => :plans, :join_table => :plans
 
     validates :nusp, numericality: true,
                         presence: {message: 'this field cannot be left black'},
                         format: {with: /[0-9]{1,8}/,
                                  message: 'is not a valid No. USP'},
                         uniqueness: {message: 'this No. USP already belongs to an user'}
-end
 
+    # For easier debugging
+    def to_s
+        "Student: #{name}"
+    end
+end
--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -13,6 +13,19 @@ class StudentTest < ActiveSupport::TestCase
             :pw_hash => '215cdbc5fa64ba878835314da7daafbfdb814a534e3eb38cfcd198f30a0cf73d',
             :pw_salt => 'b265ba01f7'
             })
+        @student.save
+    end
+
+    test "student_subject relationship" do
+        @estat = Subject.find_by!(:name => "Estatística Descritiva")
+        @prob    = Subject.find_by!(:name => "Probabilidade 1")
+        @student   = Student.find_by!(:name => "Student 1")
+        @student.subjects << @prob
+
+        # 'plans' method
+        assert     @student.subjects.exists?(@prob.id), "'#{@prob.name}' should be não sei #{@student.name}"
+        assert     @student.subjects[0].valid?, "'#{@prob.name}' should be não sei #{@student.name}"
+        assert_not @student.subjects.exists?(@estat.id), "'#{@estat.name}' should not be nao sei #{@student.name}"
     end
 
     test "should be valid" do
