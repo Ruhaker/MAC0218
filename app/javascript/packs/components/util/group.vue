07e0a289a4e94f35ad4eb1a8fd5d220c667ec7be
Victor Seiji Hariki <victorseijih@gmail.com>
2018-06-25 19:26:34 -0300

    Added code to add children
    Signed-off-by: Victor Seiji Hariki <victorseijih@gmail.com>

--- app/javascript/packs/components/util/group.vue
+++ app/javascript/packs/components/util/group.vue
@@ -4,7 +4,7 @@
       <!-- If Subject -->
       <div v-if='group_obj && !is_group' class='toolbar'>
         <div id='left'>
-          <div>{{group_obj.name}}</div>
+          <div>{{group_obj.code}} - {{group_obj.name}}</div>
         </div>
         <div class='spacer'/>
         <div id='right' class='toolbar'>
@@ -17,20 +17,25 @@
         </div>
         <div class='spacer'/>
         <div id='right' class='toolbar'>
-          <div>
-            <create-icon rootClass='toolbar-icon' w='25' h='25' />
-          </div>
-          <div>
-            <add-icon rootClass='toolbar-icon' w='25' h='25' />
+          <div v-if='true' style='display: flex'>
+            <div v-on:click="enable_editing">
+              <create-icon rootClass='header-icon' w='25' h='25' />
+            </div>
+            <div v-on:click="add_child">
+              <add-icon rootClass='header-icon' w='25' h='25' />
+            </div>
           </div>
         </div>
       </div>
       <hr v-if='group_obj && is_group'>
     </div>
+    <!-- Draw children -->
     <div v-if='group_obj'>
-      <div v-for='(child, index) in group_obj.children' v-bind:key="index">
-        <group :groupobj='child'/>
-      </div>
+      <transition-group name='children-list'>
+        <div v-for='(child, index) in group_obj.children' v-bind:key="index" class='children-list-item'>
+          <group :groupobj='child'/>
+        </div>
+      </transition-group>
     </div>
   </div>
 </template>
@@ -41,6 +46,8 @@ import auth from './auth';
 
 import AddIcon from 'vue-ionicons/dist/md-add.vue';
 import CreateIcon from 'vue-ionicons/dist/md-create.vue';
+import CheckIcon from 'vue-ionicons/dist/md-checkmark.vue';
+
 import { Chrome } from 'vue-color';
 
 export default {
@@ -49,7 +56,13 @@ export default {
   data() {
     return { groupo: null };
   },
-  components: { 'chrome-color-picker': Chrome, group, AddIcon, CreateIcon },
+  components: {
+    'chrome-color-picker': Chrome,
+    group,
+    AddIcon,
+    CreateIcon,
+    CheckIcon
+  },
   computed: {
     group_id() {
       return this.groupid;
@@ -84,19 +97,28 @@ export default {
           this.group_obj = response.data.group;
         })
         .catch(error => {});
+    },
+    enable_editing() {},
+    add_child() {
+      this.group_obj.children.push({
+        editing: true,
+        type: 'subject',
+        code: 'MSH12345',
+        name: 'Matéria daora'
+      });
     }
   }
 };
 </script>
 
 <style lang="scss" scoped>
-.toolbar-icon {
+.header-icon {
   color: #555;
   height: 20pt;
   cursor: pointer;
 }
 
-.toolbar-icon:hover {
+.header-icon:hover {
   color: #111;
 }
 
@@ -105,6 +127,22 @@ export default {
   border-radius: 15pt;
   padding: 15pt;
   margin: 15pt;
+}
+
+// List transitions
+.children-list-item {
+  transition: all 0.25s;
+  margin-right: 10px;
+}
+
+.children-list-enter,
+.children-list-leave-to {
+  opacity: 0;
+  transform: translateY(15px);
+}
+
+.children-list-leave-active {
+  position: absolute;
 }
 </style>
 
